2cc48086835b345164c9d8829f69938263101949
dmas5 <artur.unt@gmail.com>
2024-12-03 18:12:48 +0200

    app

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import { todoReducer } from './reducers/todoReducer.jsx'
+
+const store = createStore(todoReducer)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+      <Provider store={store}>
     <App />
+    </Provider>
   </StrictMode>,
 )
